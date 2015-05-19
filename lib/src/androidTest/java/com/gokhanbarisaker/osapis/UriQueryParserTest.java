2509101b2fa138a0ace6e9151d00eae1e0dfee7d
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2015-05-19 11:33:09 +0400

    Updated android sdk and plugin version

---
+++ lib/src/androidTest/java/com/gokhanbarisaker/osapis/UriQueryParserTest.java
@@ -0,0 +1,22 @@
+package com.gokhanbarisaker.osapis;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.gokhanbarisaker.osapis.utility.UriQueryParser;
+
+import junit.framework.TestCase;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by gokhanbarisaker on 4/26/15.
+ */
+@RunWith(MockitoJUnitRunner.class)
+@SmallTest
+public class UriQueryParserTest extends TestCase
+{
+    @org.junit.Test
+    public void testASDF() throws Exception {
+        assertEquals(true, false);
+    }
+}
